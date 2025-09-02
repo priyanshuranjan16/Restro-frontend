012756597185d134b27c88d5b5f1d74520527fda
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2025-09-02 16:27:16 +0530

    implemented page

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="w-full bg-white">
-      {/* Navbar */}
+      
       <Navbar />
 
       <Hero />
